ac2a1def4b3679e14095b88322609a754a833c35
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-09-28 13:11:59 +0530

    First Changes

---
+++ backend/src/index.ts
@@ -0,0 +1,37 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+let senderSocket: WebSocket | null = null;
+let receiverSocket: WebSocket | null = null;
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+  ws.on("message", function message(data: any) {
+    const message = JSON.parse(data);
+    if (message.type === "sender") {
+      senderSocket = ws;
+    } else if (message.type === "receiver") {
+      receiverSocket = ws;
+    } else if (message.type === "createOffer") {
+      receiverSocket?.send(
+        JSON.stringify({ type: "createOffer", sdp: message.sdp })
+      );
+    } else if (message.type === "createAnswer") {
+      senderSocket?.send(
+        JSON.stringify({ type: "createAnswer", sdp: message.sdp })
+      );
+    } else if (message.type === "iceCandidate") {
+      if (ws === senderSocket) {
+        receiverSocket?.send(
+          JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+        );
+      } else if (ws === receiverSocket) {
+        senderSocket?.send(
+          JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+        );
+      }
+    }
+    console.log(message);
+  });
+});
